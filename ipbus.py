9d05b226859c08a214d7f82ab6efcf46656260b9
Garrett Scott <gascott@eepp-xilinx3.physics.arizona.edu>
2024-10-01 15:33:14 -0700

    added read functionality to ipbus.py

--- ipbus.py
+++ ipbus.py
@@ -1,3 +1,19 @@
+import uhal
+''' This class is the interface for performing read and write opartions 
+    via the ipbus protocol 
+'''
 class Ipbus:
-    def __init__(self):
-        pass
+    def __init__(self, ipAddr):
+        self._manager = uhal.ConnectionManager("file://srtm_connection.xml")
+        self._hw = self._manager.getDevice("udp.srtm")
+        
+    def read(self, reg):
+        ''' Read a single register and retun the read value
+        '''
+        out = self._hw.getNode(reg)
+        self._hw.dispatch()
+        return out
+
+    def write(self, reg):
+        # TODO: implment this. 
+        pass 
