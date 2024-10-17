d4ac14b2d23e8e3dd5e8545e067a5e1db7ce54df
Alex <aponcea@arizona.edu>
2024-10-17 13:22:55 -0700

    Firmware.py update

--- FirmwareControl.py
+++ FirmwareControl.py
@@ -22,4 +22,81 @@ class FirmwareControl:
                     print(hex(srtm.read_axi_board_info_dnaHigh()))
         
         print("Exiting...")
+        while (True):
+            command = self._menu.run()
+            #if menu.run returns null, maybe change to -1, then end
+            if command==None:
+                break
+            else:
+                if command=='axi_boardinfo_efuse':
+                    for srtm in self._srtms:
+                        print(hex(srtm.read_axi_board_info_efues()))
+                elif command=='axi_boardinfo_dna_low':
+                    #for srtm in self._srtms:
+                    #  print(hex(srtm.read_axi_board_info_dnaLow()))
+                    pass
+                elif command=='axi_boardinfo_dna_middle':
+                    #for srtm in self._srtms:
+                    #  print(hex(srtm.read_axi_board_info_dnaMiddle()))
+                    pass
+                elif command=='axi_boardinfo_dna_high':
+                    for srtm in self._srtms:
+                      print(hex(srtm.read_axi_board_info_dnaHigh()))
+                elif command=='axi_boardinfo_user_reg0':
+                    pass
+                elif command=='axi_boardinfo_user_reg1':
+                    pass
+                elif command=='axi_boardinfo_user_reg2':
+                    pass
+                elif command=='axi_boardinfo_user_reg3':
+                    pass
+                elif command=='axi_boardinfo_user_reg4':
+                    pass
+                elif command=='axi_boardinfo_user_reg5':
+                    pass
+                elif command=='axi_boardinfo_user_reg6':
+                    pass
+                elif command=='axi_boardinfo_user_reg7':
+                    pass
+                elif command=='freq_count_ctrl_reg':
+                    pass
+                elif command=='freq_count_max_cnt':
+                    pass
+                elif command=='freq_count_base':
+                    pass
+                elif command=='freq_count_clk0':
+                    pass
+                elif command=='freq_count_clk1':
+                    pass
+                elif command=='freq_count_clk2':
+                    pass
+                elif command=='freq_count_clk3':
+                    pass
+                elif command=='freq_count_clk4':
+                    pass
+                elif command=='freq_count_clk5':
+                    pass
+                elif command=='freq_count_clk6':
+                    pass
+                elif command=='freq_count_clk7':
+                    pass
+                elif command=='freq_count_clk8':
+                    pass
+                elif command=='freq_count_clk9':
+                    pass
+                elif command=='freq_count_clk10':
+                    pass
+                elif command=='freq_count_clk11':
+                    pass
+                elif command=='freq_count_clk12':
+                    pass
+                elif command=='freq_count_clk13':
+                    pass
+                elif command=='freq_count_clk14':
+                    pass
+                elif command=='freq_count_clk15':
+                    pass
+                elif command=='freq_count_clk16':
+                    pass
+                #expand as SRTM is more complete
 
