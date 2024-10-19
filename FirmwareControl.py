78e08209623c520052673b81be2559741fe1d1f5
Alex <aponcea@arizona.edu>
2024-10-19 16:23:12 -0700

    Added read/write options for board registers

--- FirmwareControl.py
+++ FirmwareControl.py
@@ -25,78 +25,94 @@ class FirmwareControl:
         while (True):
             command = self._menu.run()
             #if menu.run returns null, maybe change to -1, then end
-            if command==None:
+            if command==0:
                 break
             else:
-                if command=='axi_boardinfo_efuse':
-                    for srtm in self._srtms:
-                        print(hex(srtm.read_axi_board_info_efues()))
-                elif command=='axi_boardinfo_dna_low':
-                    #for srtm in self._srtms:
-                    #  print(hex(srtm.read_axi_board_info_dnaLow()))
-                    pass
-                elif command=='axi_boardinfo_dna_middle':
-                    #for srtm in self._srtms:
-                    #  print(hex(srtm.read_axi_board_info_dnaMiddle()))
-                    pass
-                elif command=='axi_boardinfo_dna_high':
-                    for srtm in self._srtms:
-                      print(hex(srtm.read_axi_board_info_dnaHigh()))
-                elif command=='axi_boardinfo_user_reg0':
-                    pass
-                elif command=='axi_boardinfo_user_reg1':
-                    pass
-                elif command=='axi_boardinfo_user_reg2':
-                    pass
-                elif command=='axi_boardinfo_user_reg3':
-                    pass
-                elif command=='axi_boardinfo_user_reg4':
-                    pass
-                elif command=='axi_boardinfo_user_reg5':
-                    pass
-                elif command=='axi_boardinfo_user_reg6':
-                    pass
-                elif command=='axi_boardinfo_user_reg7':
-                    pass
-                elif command=='freq_count_ctrl_reg':
-                    pass
-                elif command=='freq_count_max_cnt':
-                    pass
-                elif command=='freq_count_base':
-                    pass
-                elif command=='freq_count_clk0':
-                    pass
-                elif command=='freq_count_clk1':
-                    pass
-                elif command=='freq_count_clk2':
-                    pass
-                elif command=='freq_count_clk3':
-                    pass
-                elif command=='freq_count_clk4':
-                    pass
-                elif command=='freq_count_clk5':
-                    pass
-                elif command=='freq_count_clk6':
-                    pass
-                elif command=='freq_count_clk7':
-                    pass
-                elif command=='freq_count_clk8':
-                    pass
-                elif command=='freq_count_clk9':
-                    pass
-                elif command=='freq_count_clk10':
-                    pass
-                elif command=='freq_count_clk11':
-                    pass
-                elif command=='freq_count_clk12':
-                    pass
-                elif command=='freq_count_clk13':
-                    pass
-                elif command=='freq_count_clk14':
-                    pass
-                elif command=='freq_count_clk15':
-                    pass
-                elif command=='freq_count_clk16':
-                    pass
-                #expand as SRTM is more complete
+                    if command=='axi_boardinfo_efuse':
+                        for srtm in self._srtms:
+                            print(hex(srtm.read_axi_board_info_efues()))
+                    elif command=='axi_boardinfo_dna_low':
+                        for srtm in self._srtms:
+                          print(hex(srtm.read_axi_board_info_low()))
+                        pass
+                    elif command=='axi_boardinfo_dna_middle':
+                        for srtm in self._srtms:
+                          print(hex(srtm.read_axi_board_info_middle()))
+                        pass
+                    elif command=='axi_boardinfo_dna_high':
+                        for srtm in self._srtms:
+                         print(hex(srtm.read_axi_board_info_dnaHigh()))
+                    elif command=='axi_boardinfo_user_reg0':
+                        print(hex(srtm.read_axi_boardinfo_usernum(0)))
+                    elif command=='axi_boardinfo_user_reg1':
+                        print(hex(srtm.read_axi_boardinfo_usernum(1)))
+                    elif command=='axi_boardinfo_user_reg2':
+                        print(hex(srtm.read_axi_boardinfo_usernum(2)))
+                    elif command=='axi_boardinfo_user_reg3':
+                        print(hex(srtm.read_axi_boardinfo_usernum(3)))
+                    elif command=='axi_boardinfo_user_reg4':
+                        print(hex(srtm.read_axi_boardinfo_usernum(4)))
+                    elif command=='axi_boardinfo_user_reg5':
+                        if getReadWrite()==1:
+                            print(hex(srtm.read_axi_boardinfo_usernum(5)))
+                        else:
+                            print(hex(srtm.write_axi_boardinfo_usernum(5)))
+                    elif command=='axi_boardinfo_user_reg6':
+                        if getReadWrite()==1:
+                            print(hex(srtm.read_axi_boardinfo_usernum(6)))
+                        else:
+                            print(hex(srtm.write_axi_boardinfo_usernum(6)))
+                    elif command=='axi_boardinfo_user_reg7':
+                        if getReadWrite()==1:
+                            print(hex(srtm.read_axi_boardinfo_usernum(7)))
+                        else:
+                            print(hex(srtm.write_axi_boardinfo_usernum(7)))
+                    elif command=='freq_count_ctrl_reg':
+                        pass
+                    elif command=='freq_count_max_cnt':
+                        pass
+                    elif command=='freq_count_base':
+                        pass
+                    elif command=='freq_count_clk0':
+                        pass
+                    elif command=='freq_count_clk1':
+                        pass
+                    elif command=='freq_count_clk2':
+                        pass
+                    elif command=='freq_count_clk3':
+                        pass
+                    elif command=='freq_count_clk4':
+                        pass
+                    elif command=='freq_count_clk5':
+                        pass
+                    elif command=='freq_count_clk6':
+                        pass
+                    elif command=='freq_count_clk7':
+                        pass
+                    elif command=='freq_count_clk8':
+                        pass
+                    elif command=='freq_count_clk9':
+                        pass
+                    elif command=='freq_count_clk10':
+                        pass
+                    elif command=='freq_count_clk11':
+                        pass
+                    elif command=='freq_count_clk12':
+                        pass
+                    elif command=='freq_count_clk13':
+                        pass
+                    elif command=='freq_count_clk14':
+                        pass
+                    elif command=='freq_count_clk15':
+                        pass
+                    elif command=='freq_count_clk16':
+                        pass
+                    #expand as SRTM is more complete
+
+def getReadWrite(self):
+    ret=0
+    while(ret<1 or ret>2):
+        print("Would you like to read (1) or write (2)?")
+        ret = int(input(">").strip())
+    return ret
 
