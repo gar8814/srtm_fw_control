4d0cbc9e42d1a96dab97ae3e0f10aed159aebdc5
Parker-Hines <146996814+Parker-Hines@users.noreply.github.com>
2024-10-19 15:24:57 -0700

    Update Menu.py
    Updated exit return code, and cleaned up menu options.

--- Menu.py
+++ Menu.py
@@ -5,12 +5,14 @@ class Menu:
         self.menuTree = {
             "Main Menu": {
                 "options": ["Board Info", "Frequency Counter"],
+                "prefix": "",
                 "parent": None
             },
             "Board Info": {
                 "options": ['axi_boardinfo_efuse', 'axi_boardinfo_dna_low', 'axi_boardinfo_dna_middle', 'axi_boardinfo_dna_high', 'axi_boardinfo_user_reg0', 
                             'axi_boardinfo_user_reg1', 'axi_boardinfo_user_reg2', 'axi_boardinfo_user_reg3', 'axi_boardinfo_user_reg4', 
                             'axi_boardinfo_user_reg5', 'axi_boardinfo_user_reg6', 'axi_boardinfo_user_reg7'],
+                "prefix": "axi_boardinfo_",
                 "parent": "Main Menu"
             },
             "Frequency Counter": {
@@ -18,6 +20,7 @@ class Menu:
                             'freq_count_clk4', 'freq_count_clk5', 'freq_count_clk6', 'freq_count_clk7', 'freq_count_clk8', 'freq_count_clk9', 
                             'freq_count_clk10', 'freq_count_clk11', 'freq_count_clk12', 'freq_count_clk13', 'freq_count_clk14', 'freq_count_clk15', 
                             'freq_count_clk16'],
+                "prefix": "freq_count_",
                 "parent": "Main Menu"
             }
         }
@@ -30,7 +33,7 @@ class Menu:
             if choice == 0:
                 if self.currentMenu == "Main Menu":
                     print("Exiting...")
-                    break
+                    return 0
                 else:
                     self.currentMenu = self.menuTree[self.currentMenu]["parent"]
             else:
@@ -49,7 +52,8 @@ class Menu:
         print(f"{self.currentMenu}:")
         options = self.menuTree[self.currentMenu]["options"]
         for index, option in enumerate(options, start=1):
-            print(f"\t{index}. {option}")
+            suffix = option[len(self.menuTree[self.currentMenu]["prefix"]):]
+            print(f"\t{index}. {suffix}")
         print("\t0. " + returnOption)
     
     def _getSelection(self, options):
