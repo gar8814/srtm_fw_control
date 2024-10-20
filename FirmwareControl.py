e4487c0b6cf5c0bdb3ff8ff7af50d2c059a36c22
Garrett Scott <gascott@eepp-xilinx3.physics.arizona.edu>
2024-10-20 12:21:57 -0700

    fixed some minor bugs

--- FirmwareControl.py
+++ FirmwareControl.py
@@ -7,16 +7,23 @@ class FirmwareControl:
         self._srtms.append(SRTM("117", debug))
         self._menu = Menu()
 
+    def getReadWrite(self):
+        ret=0
+        while(ret<1 or ret>2):
+            print("Would you like to read (1) or write (2)?")
+            ret = int(input(">").strip())
+        return ret
+    
     def run(self):
-        while (True):
-            print("SRTM Firmware Control:")
-            print("1. Read Board Info DNA High")
-            usr_input = input("Select an option:(0 to exit)")
-            if usr_input == '0':
-                break
-            else:
-                for srtm in self._srtms:
-                    print(hex(srtm.read_axi_board_info_dnaHigh()))
+        # while (True):
+        #     print("SRTM Firmware Control:")
+        #     print("1. Read Board Info DNA High")
+        #     usr_input = input("Select an option:(0 to exit)")
+        #     if usr_input == '0':
+        #         break
+        #     else:
+        #         for srtm in self._srtms:
+        #             print(hex(srtm.read_axi_board_info_dnaHigh()))
         
         
 
@@ -39,32 +46,43 @@ class FirmwareControl:
                         pass
                     elif command=='axi_boardinfo_dna_high':
                         for srtm in self._srtms:
-                         print(hex(srtm.read_axi_board_info_dnaHigh()))
+                             print(hex(srtm.read_axi_board_info_dnaHigh()))
                     elif command=='axi_boardinfo_user_reg0':
-                        print(hex(srtm.read_axi_boardinfo_usernum(0)))
+                        for srtm in self._srtms:
+                            print(hex(srtm.read_axi_boardinfo_usernum(0)))
                     elif command=='axi_boardinfo_user_reg1':
-                        print(hex(srtm.read_axi_boardinfo_usernum(1)))
+                        for srtm in self._srtms:
+                            print(hex(srtm.read_axi_boardinfo_usernum(1)))
                     elif command=='axi_boardinfo_user_reg2':
-                        print(hex(srtm.read_axi_boardinfo_usernum(2)))
+                        for srtm in self._srtms:
+                            print(hex(srtm.read_axi_boardinfo_usernum(2)))
                     elif command=='axi_boardinfo_user_reg3':
-                        print(hex(srtm.read_axi_boardinfo_usernum(3)))
+                        for srtm in self._srtms:
+                            print(hex(srtm.read_axi_boardinfo_usernum(3)))
                     elif command=='axi_boardinfo_user_reg4':
-                        print(hex(srtm.read_axi_boardinfo_usernum(4)))
+                        for srtm in self._srtms:
+                            print(hex(srtm.read_axi_boardinfo_usernum(4)))
                     elif command=='axi_boardinfo_user_reg5':
-                        if getReadWrite()==1:
-                            print(hex(srtm.read_axi_boardinfo_usernum(5)))
+                        if self.getReadWrite()==1:
+                            for srtm in self._srtms:
+                                print(hex(srtm.read_axi_boardinfo_usernum(5)))
                         else:
-                            print(hex(srtm.write_axi_boardinfo_usernum(5)))
+                            for srtm in self._srtms:
+                                print(hex(srtm.write_axi_boardinfo_usernum(5)))
                     elif command=='axi_boardinfo_user_reg6':
-                        if getReadWrite()==1:
-                            print(hex(srtm.read_axi_boardinfo_usernum(6)))
+                        if self.getReadWrite()==1:
+                            for srtm in self._srtms:
+                                print(hex(srtm.read_axi_boardinfo_usernum(6)))
                         else:
-                            print(hex(srtm.write_axi_boardinfo_usernum(6)))
+                            for srtm in self._srtms:
+                                print(hex(srtm.write_axi_boardinfo_usernum(6)))
                     elif command=='axi_boardinfo_user_reg7':
-                        if getReadWrite()==1:
-                            print(hex(srtm.read_axi_boardinfo_usernum(7)))
+                        if self.getReadWrite()==1:
+                            for srtm in self._srtms:
+                                print(hex(srtm.read_axi_boardinfo_usernum(7)))
                         else:
-                            print(hex(srtm.write_axi_boardinfo_usernum(7)))
+                            for srtm in self._srtms:
+                                print(hex(srtm.write_axi_boardinfo_usernum(7)))
                     elif command=='freq_count_ctrl_reg':
                         pass
                     elif command=='freq_count_max_cnt':
@@ -105,12 +123,4 @@ class FirmwareControl:
                         pass
                     elif command=='freq_count_clk16':
                         pass
-                    #expand as SRTM is more complete
-       print("Exiting...")  
-
-def getReadWrite(self):
-    ret=0
-    while(ret<1 or ret>2):
-        print("Would you like to read (1) or write (2)?")
-        ret = int(input(">").strip())
-    return ret
+                    #expand as SRTM is more complete  
