4141a73d0a689514bc4e321f49ce8a3a373397ac
Alex <aponcea@arizona.edu>
2024-11-07 14:45:02 -0700

    Reads all registers/counters and can write

--- FirmwareControl.py
+++ FirmwareControl.py
@@ -19,20 +19,30 @@ class FirmwareControl:
         ret = input(">").strip()
         return ret
 
+    def LTIcase(self):
+        print("Enter case number")
+        ret = input(">").strip()
+        return ret
+
     def run(self):
         while (True):
             command = self._menu.run()
             if command==0:
                 break
-            elif command=="read":
-                #readAllcommand()
-                #loop through dictionary or return data structure
-                    #print(hex())
-                pass
-            elif command=="write":
-                if self.getReadWrite()==1:
+            elif command=="Read Board Info":
+                SRTM.read_all_boards()
+            elif command=="Read Frequency Counter":
+                SRTM._read_each_clk()
+            elif command=="Run Test A":
+                case = self.LTIcase()
+                print(f'LTI {case} runs here')
+            elif command=="Run Test B":
+                case = self.LTIcase()
+                print(f'LTI {case} runs here')
+            else:
+                #returns reg name
+                if command == 'freq_count_max_cnt':
+                    pass
+                elif self.getReadWrite()==1:
                     msg=self.writeMsg()
-                pass
-            elif command=="LTI":
-                # do specified test
-                pass
+                    SRTM.write_axi_boardinfo_usernum(command,msg)
