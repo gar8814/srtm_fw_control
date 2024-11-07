0e7ca989b28c27c147489a3d633255c0b02d3b2b
Garrett Scott <gascott@eepp-xilinx3.physics.arizona.edu>
2024-11-07 13:05:30 -0700

    added lti_send_test_data() to SRTM.py

--- SRTM.py
+++ SRTM.py
@@ -1,4 +1,5 @@
 from Ipbus import Ipbus
+import time
 
 class SRTM:
 
@@ -63,3 +64,102 @@ class SRTM:
             reg = self.read_board(i)
             print(f'userreg {i}:  {reg}')
         return
+    
+    def lti_send_test_data(self, case):
+        try:
+            f = open(f'lit_data/lti_input_data_case{case}.dat','r')
+            g = open(f'lit_data/lti_input_data_case{case}.dat','r')
+
+            wait = 0.2
+            linecount = 0
+            for line in f:
+                linecout += 1
+
+            print('lti reset 1')
+            self._ipbus.write('lti_control_reg.reset', 1)
+            time.sleep(5)
+
+            print('lti reset 0')
+            self._ipbus.write('lti_control_reg.reset', 0)
+            time.sleep(5)
+
+            print('lti loopback 0')
+            self._ipbus.write("lti_control_reg.loopback", 0)
+            time.sleep(wait)
+
+            print('lti loopback 1')
+            self._ipbus.write("lti_control_reg.loopback", 1)
+            time.sleep(wait)
+
+            print('lti loopback 0')
+            self._ipbus.write("lti_control_reg.loopback", 0)
+            time.sleep(wait)
+
+            print('lti frame source 1')
+            self._ipbus.write("lti_control_reg.frame_source", 1)
+            time.sleep(wait)
+
+            print('lti frame source 0')
+            self._ipbus.write("lti_control_reg.frame_source", 0)
+            time.sleep(wait)
+
+            print('lti frame source 1')
+            self._ipbus.write("lti_control_reg.frame_source", 1)
+            time.sleep(wait)
+
+            max_words = linecount
+            print(f"write max_words = {max_words}")
+            self._ipbus.write("lti_max_words", max_words)
+            time.sleep(wait)
+
+            print ('writing tx fifo data and charisk')
+            for i in range(linecount):
+                s = f.readline().split()
+                lti_data = int(s[0],16)
+                s = g.readline().split()
+                lti_kchar = int(s[0], 16)
+                self._ipbus.write("lti_tx_fifo_charisk", lti_kchar)
+                time.sleep(wait)
+                self._ipbus.write("lti_tx_fifo_data", lti_data)
+                time.sleep(wait)
+            
+            print ('read tx fifo word count')
+            reg0 = self._ipbus.read("lti_tx_word_cnt")
+            time.sleep(wait)
+            print("lti_tx_word_cnt", hex(reg0))
+
+            print ('read rx fifo word count')
+            reg0 = self._ipbus.read("lti_rx_word_cnt")
+            time.sleep(wait)
+            print("lti_rx_word_cnt", hex(reg0))
+
+            print ('send to 1')
+            send = 1
+            self._ipbus.write("lti_control_reg.send", send)
+            time.sleep(wait)
+
+            print ('send to 0')
+            send = 0
+            self._ipbus.write("lti_control_reg.send", send)
+            time.sleep(wait)
+
+            print ('read tx fifo word count')
+            reg0 = self._ipbus.read("lti_tx_word_cnt")
+            time.sleep(wait)
+            print("lti_tx_word_cnt", hex(reg0))
+
+            print ('read rx fifo word count')
+            reg0 = self._ipbus.read("lti_rx_word_cnt")
+            time.sleep(wait)
+            print("lti_rx_word_cnt", hex(reg0))
+
+            f.close()
+            g.close()
+            print ('done!')
+
+
+        except:
+            print('Bad Case')
+        
+    def _do_lti_case_1():
+        pass
