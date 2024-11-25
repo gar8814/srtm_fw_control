7048cf1274de9acbed615fbbe01f59b2d3f58099
Parker-Hines <phines@arizona.edu>
2024-11-25 00:08:12 -0700

    Added CLI for srtml_fw_control
    also added some modularization to FirmwareControl, necessary for functionality.

--- FirmwareControl.py
+++ FirmwareControl.py
@@ -1,6 +1,7 @@
 from SRTM import SRTM
 from Menu import Menu
 
+
 class FirmwareControl:
     def __init__(self, debug=False):
         self._srtms = []
@@ -8,18 +9,18 @@ class FirmwareControl:
         self._menu = Menu()
 
     def getReadWrite(self):
-        ret=0
-        while(ret<1 or ret>2):
+        ret = 0
+        while (ret < 1 or ret > 2):
             print("Would you like to read (1) or write (2)?")
             ret = int(input(">").strip())
         return ret
-    
+
     def writeMsg(self):
         print("Enter new value")
         ret = input(">").strip()
         return ret
 
-    def _LTIcase(self):
+    def LTIcase(self):
         print("Enter case number")
         ret = input(">").strip()
         return ret
@@ -27,32 +28,52 @@ class FirmwareControl:
     def run(self):
         while (True):
             command = self._menu.run()
-            if command==0:
+            if command == 0:
                 break
-            elif command=="Read Board Info":
-                for i in range(len(self._srtms)):
-                    self._srtms[i].read_all_boards()
+            elif command == "Read Board Info":
+                self.handleReadBoardInfo()
 
-            elif command=="Read Frequency Counter":
-                for i in range(len(self._srtms)):
-                    self._srtms[i].read_all_clk()
+            elif command == "Read Frequency Counter":
+                self.handleReadFreqCounter()
 
-            elif command=="Run Test A":
-                case = self._LTIcase()
-                print(f'LTI {case} runs here')
-                for i in range(len(self._srtms)):
-                    self._srtms[i].lti_send_test_data(case)
+            elif command == "Run Test A":
+                self.handleLTITestA()
 
-            elif command=="Run Test B":
-                case = self._LTIcase()
-                print(f'LTI {case} runs here')
-                #SRTM.lti_send_test_data(case)
-            #returns reg name
+            elif command == "Run Test B":
+                self.handleLTITestB()
+            # returns reg name
             elif command == 'freq_count_max_cnt':
-                print("INFO: ")
-                if self.getReadWrite()==1:
-                    msg=self.writeMsg()
-                    for i in range(len(self._srtms)):
-                        self._srtms[i].write_axi_boardinfo_usernum(command,msg)
+                self.handleFreqMaxCount(command)
             else:
-                print(f'ERROR: {command} invalid:')
+                self.handleWriteBoardInfo(command)
+
+    def handleReadBoardInfo(self):
+        for i in range(len(self._srtms)):
+            self._srtms[i].read_all_boards()
+
+    def handleReadFreqCounter(self):
+        for i in range(len(self._srtms)):
+            self._srtms[i].read_all_clk()
+
+    def handleLTITestA(self):
+        case = self.LTIcase()
+        print(f'LTI {case} runs here')
+        for i in range(len(self._srtms)):
+            self._srtms[i].lti_send_test_data(case)
+
+    def handleWriteBoardInfo(self, command):
+        msg = self.writeMsg()
+        for i in range(len(self._srtms)):
+            self._srtms[i].write_axi_boardinfo_usernum(command, msg)
+
+    def handleLTITestB(self):
+        case = self.LTIcase()
+        print(f'LTI {case} runs here')
+        # SRTM.lti_send_test_data(case)
+
+    def handleFreqMaxCount(self, command):
+        print("INFO: ")
+        if self.getReadWrite() == 1:
+            msg = self.writeMsg()
+            for i in range(len(self._srtms)):
+                self._srtms[i].write_axi_boardinfo_usernum(command, msg)
--- srtm_fw_control.py
+++ srtm_fw_control.py
@@ -4,9 +4,55 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--debug', default=False, help='Use fake ipbus')
+    parser.add_argument('-m', '--mode', choices=['write', 'read', 'run'], help='Mode of operation: write, read, or run')
+    parser.add_argument('sub', nargs='?', default=None, help='Submenu for selecting a reg, e.g. "board" or "freq"')
+    parser.add_argument('reg', nargs='?', default=None, help='Register for use in write, read, or run modes')
+    parser.add_argument('series', nargs='?', default=None, help='Test series, e.g. A or B')
     args = parser.parse_args()
     instance = FirmwareControl(args.debug)
-    instance.run()
+    if args.mode == 'write':
+        if args.sub is None or args.reg is None:
+            print("Syntax: python3 srtm_fw_control.py -m write <Subsystem> <Register>")
+        else:
+            if args.sub == 'board':
+                instance.handleWriteBoardInfo(args.reg)
+            elif args.sub == 'freq':
+                instance.handleWriteFreqInfo(args.reg)
+            else:
+                print("Invalid write option: Try \"board\" or \"freq\".")
+    elif args.mode == 'read':
+        if args.sub is None or args.reg is None:
+            print("Syntax: python3 srtm_fw_control.py -m read <Subsystem> <Register>")
+        else:
+            if args.sub.lower() == 'board':
+                instance.handleReadBoardInfo()
+            elif args.sub.lower() == 'freq':
+                instance.handleReadFreqInfo()
+            else:
+                print("Invalid subsystem: Try \"board\" or \"freq\".")
+    elif args.mode == 'run':
+        if args.sub is None or args.reg is None or args.series is None:
+            print("Syntax: python3 srtm_fw_control.py -m run <Test Unit> test <Series>")
+        else:
+            if args.sub.lower() == 'lti':
+                if args.reg.lower() == 'test':
+                    if args.series.lower() == 'a':
+                        instance.handleLTITestA()
+                    elif args.series.lower() == 'b':
+                        instance.handleLTITestB()
+                    else:
+                        print("Invalid test series, try A or B.")
+                else:
+                    print("Syntax: python3 srtm_fw_control.py -m run <Test Unit> test <Series>")
+            else:
+                print("Syntax: python3 srtm_fw_control.py -m run LTI test <Series>")
+    else:
+        if args.sub is not None:
+            print('Argument detected, but you did not enter a mode:\n')
+            print("Syntax: python3 srtm_fw_control.py -m <Mode> <Group> <Reg>\n")
+            print("Example: python3 srtm_fw_control.py -m write board axi_boardinfo_user_reg5")
+        else:
+            instance.run()
 
 if __name__ == "__main__":
     main()
