3bc6bc5ccbdc4543a13f27185984999f243cd2c0
gar8814 <garrettscott@arizona.edu>
2024-12-03 16:10:58 -0700

    commented out un used parser.add_argument

--- srtm_fw_control.py
+++ srtm_fw_control.py
@@ -4,8 +4,8 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--debug', default=False, help='Use fake ipbus')
-    parser.add_argument('-S', '--subsystem', help='Select subsystem to command ie, "AXI_BOARD_INFO, FEQ, LTI, SPI')
-    parser.add_argument('-c', '--command', help='Select the command to be run ie, "READ, RUN ')
+    # parser.add_argument('-S', '--subsystem', help='Select subsystem to command ie, "AXI_BOARD_INFO, FEQ, LTI, SPI')
+    # parser.add_argument('-c', '--command', help='Select the command to be run ie, "READ, RUN ')
     args = parser.parse_args()
     instance = FirmwareControl(args.debug)
     instance.run()
